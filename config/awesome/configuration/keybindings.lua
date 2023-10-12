ab9c97b3c6b9b34051869012f609a5fbd7e59fb7
Eugene Frolov <spanditime@gmail.com>
2023-10-12 23:14:00 +0300

    Add fish aliases, fix autorun script and add russian layout, make kb mod4+l for locking

--- config/awesome/configuration/keybindings.lua
+++ config/awesome/configuration/keybindings.lua
@@ -12,6 +12,9 @@ awful.keyboard.append_global_keybindings({
               {description="show help", group="awesome"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
+
+    awful.key({ modkey   }, "l", function () awful.spawn("xautolock -locknow") end,
+              {description = "lock", group = "awesome"}),
     -- awful.key({ modkey, "Shift"   }, "q", awesome.quit,
     --           {description = "quit awesome", group = "awesome"}),
 })
--- config/awesome/scripts/autorun.sh
+++ config/awesome/scripts/autorun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-exec kmonad /home/spanditime/.config/kmonad/qwerty_ext.kbd
+exec setxkbmap -layout us,ru -option grp:caps_toggle 
 exec xautolock -time 10 -locker slock -nowlocker slock 
 exec playerctld daemon 
+
+exec kmonad /home/spanditime/.config/kmonad/qwerty_ext.kbd
